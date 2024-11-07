eee41faef22cba2eb18153d0a9af70fc425d3b15
preeti9667 <saini04preeti@gmail.com>
2024-11-07 10:56:24 +0530

    initial commint

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+var cookieParser = require("cookie-parser");
+var createError = require("http-errors");
+
+const app = express();
+const port = 3000;
+
+ 
+var usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
