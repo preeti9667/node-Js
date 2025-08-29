1de5aee0ce721d709e0efe4e13d3674539285413
preeti9667 <saini04preeti@gmail.com>
2025-08-29 14:02:23 +0530

    changed attendance list

--- src/app/controllers/attendance.controllers.js
+++ src/app/controllers/attendance.controllers.js
@@ -142,8 +142,8 @@ async function getAllUsers(req, res, next) {
 }
 
 async function allAttendanceList(req, res, next) {
-  const limit = Number(req.query.limit) || 10;
-  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit);
+  const page = Number(req.query.page)|| 1;
   const skip = (page - 1) * limit;
 
   const result = await AttendanceModel.aggregate([
@@ -172,9 +172,9 @@ async function allAttendanceList(req, res, next) {
           {
             $skip: skip,
           },
-          {
-            $limit: limit,
-          },
+          // {
+          //   $limit: limit,
+          // },
           {
             $project: {
               _id: 1,
@@ -190,6 +190,15 @@ async function allAttendanceList(req, res, next) {
               noOfParticipants: {
                 $size: "$participantIds",
               },
+              participantIds:
+                {
+                  $cond: {
+                    if: { $gt: ["$participantIds", []] },
+                    then: "$participantIds",
+                    else: []
+                  }
+                }
+                  
             },
           },
         ],
@@ -211,14 +220,14 @@ async function allAttendanceList(req, res, next) {
   res.status(200).json({
     status: 200,
     message: "Attendance list",
-    data: { count, list: attendance },
+    data: { count, list: attendance, },
   });
 }
 
 async function deleteAttendanceDate(req, res, next) {
   try {
-    const date = req.params.date;
-    const attendance = await AttendanceModel.findOneAndDelete({ date });
+    const id = req.params.id;
+    const attendance = await AttendanceModel.findOneAndDelete({ _id: id });
     if (!attendance) {
       return res.status(404).json({ message: "Attendance not found." });
     }
